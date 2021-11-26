b97cafe656fa507db2daa0a9275c3c8d241a80ef
Perfect Obumnemne <perfectobumneme@hotmail.com>
2021-11-26 14:41:59 -0600

    Initial Commit

---
+++ Github/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Github
+//
+//  Created by Perfect Obumneme on 11/26/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
